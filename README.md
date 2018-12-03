847faedf3f0bbd08dc06fb143623584229cd67e6
José Ernesto Barbosa <jose.barbosa@cwi.com.br>
2018-12-03 18:30:25 -0200

    Ajustes para o TDC

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Repositório para ensinar e aprender o básico sobre WireMock
 
 O WireMock é uma ferramenta HTTP mock server, onde entrega requisições estáticas para diferentes end-points.
+
+Para executar o serviço pode-se utilizar o Gitbash com o comando `sh ./server-standalone/start.sh` ou então o comando java `java -cp "server-standalone/wiremock-standalone-2.11.0.jar;server-standalone/wiremock-body-transformer-1.1.6.jar" com.github.tomakehurst.wiremock.standalone.WireMockServerRunner --verbose --https-port=8081 --extensions com.opentable.extension.BodyTransformer`.
+
+O serviço está configurado para ser executado nas portas 8080 (http) e 8081 (https).
--- server-standalone/start.sh
+++ server-standalone/start.sh
@@ -2,7 +2,9 @@
 if [[ "$OSTYPE" == "darwin"* || "$OSTYPE" == "linux-gnu" ]]; then
     java -cp "server-standalone/wiremock-standalone-2.11.0.jar:server-standalone/wiremock-body-transformer-1.1.6.jar" com.github.tomakehurst.wiremock.standalone.WireMockServerRunner --verbose --extensions com.opentable.extension.BodyTransformer
 elif [[ "$OSTYPE" == "win32" || "$OSTYPE" == "msys" ]]; then
-	java -cp "server-standalone/wiremock-standalone-2.11.0.jar;server-standalone/wiremock-body-transformer-1.1.6.jar" com.github.tomakehurst.wiremock.standalone.WireMockServerRunner --verbose --extensions com.opentable.extension.BodyTransformer
+	java -cp "server-standalone/wiremock-standalone-2.11.0.jar;server-standalone/wiremock-body-transformer-1.1.6.jar" com.github.tomakehurst.wiremock.standalone.WireMockServerRunner --verbose --https-port=8081 --extensions com.opentable.extension.BodyTransformer
 else
     echo "Yo no lo coñosco señor..."
 fi
+
+# --proxy-all=http://www.thedevelopersconference.com.br --record-mappings
